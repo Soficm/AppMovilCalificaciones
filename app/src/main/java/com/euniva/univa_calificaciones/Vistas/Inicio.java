13286e21a28d876c9d2123710b7ba817df54b347
Soficm <sofi_casillas@hotmail.com>
2020-03-31 23:22:53 -0600

    Pantalla de inicio

---
+++ app/src/main/java/com/euniva/univa_calificaciones/Vistas/Inicio.java
@@ -0,0 +1,71 @@
+package com.euniva.univa_calificaciones.Vistas;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import com.euniva.univa_calificaciones.R;
+
+public class Inicio extends AppCompatActivity {
+
+    RadioButton rbtAlumno, rbtDocente;
+    Spinner campus;
+    private Button btnSiguiente;
+    private RadioGroup rgpGrupo;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_inicio);
+
+        campus = findViewById(R.id.spn_campus);
+        rbtAlumno = findViewById(R.id.rbt_Alumno);
+        rbtDocente = findViewById(R.id.rbt_Docente);
+        btnSiguiente = findViewById(R.id.btn_siguiente);
+        rgpGrupo = findViewById(R.id.rgp_Grupo);
+
+        btnSiguiente.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                boolean vacio = BuscarVacio();
+
+                if (!vacio) {
+                    if (rbtAlumno.isChecked()) {
+                        Intent intent = new Intent(getApplicationContext(), LoginAlumno.class);
+                        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                        startActivity(intent);
+                        finish();
+                    } else if (rbtDocente.isChecked()) {
+                        Intent intent = new Intent(getApplicationContext(), LoginDocente.class);
+                        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                        startActivity(intent);
+                        finish();
+                    }
+                }
+            }
+        });
+    }
+
+    private boolean BuscarVacio() {
+
+        boolean vacio = false;
+
+        if (campus.getSelectedItemPosition() == 0) {
+            Toast.makeText(getApplicationContext(), "Seleccione un campus", Toast.LENGTH_SHORT).show();
+        } else if (rgpGrupo.getCheckedRadioButtonId() == -1) {
+            Toast.makeText(getApplicationContext(), "Seleccione un tipo de usuario", Toast.LENGTH_SHORT).show();
+        }
+        return vacio;
+    }
+}
+
---
+++ app/src/main/java/com/euniva/univa_calificaciones/Vistas/LoginDocente.java
@@ -0,0 +1,16 @@
+package com.euniva.univa_calificaciones.Vistas;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.euniva.univa_calificaciones.R;
+
+public class LoginDocente extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_docente);
+    }
+}
